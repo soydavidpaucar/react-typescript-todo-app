dd96784f424fefc199c060bb79d1980ca7f3c6c5
David Paucar <soydavidpaucar@gmail.com>
2022-11-30 21:06:48 -0500

    feat: add poppins font to the application

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss')} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
