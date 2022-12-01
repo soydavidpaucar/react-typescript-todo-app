75fd4cb22f3923e66b4448a78d7b6c77bdb5fbd8
David Paucar <soydavidpaucar@gmail.com>
2022-11-30 22:32:51 -0500

    style: update text color and margins

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -4,7 +4,7 @@ type PageTitleProps = {
 
 function PageTitle({ children }: PageTitleProps) {
   return (
-    <h1 className="inline-block w-full text-[40px] font-bold text-[#64ffda] uppercase text-center m-auto mt-[32px] mb-[24px]">
+    <h1 className="inline-block w-full text-[40px] font-bold text-[#ccd6f6] uppercase text-center m-auto mt-[20px] mb-[15px]">
       {children}
     </h1>
   );
