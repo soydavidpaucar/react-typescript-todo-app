e3dce2e861ab28ebd8196fb1aef7c9118c6ea0cf
David Paucar <soydavidpaucar@gmail.com>
2022-11-30 22:55:21 -0500

    chore: add AppHeader component and styled its wrapper div

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import PageTitle from './components/PageTitle';
+import AppHeader from './components/AppHeader';
 
 function App() {
   return (
     <div className="bg-[#0a192f]">
       <div className="max-w-7xl w-[90%] m-auto">
         <PageTitle>TODO LIST</PageTitle>
+        <div className="w-full max-w-[750px] m-auto">
+          <AppHeader />
+        </div>
       </div>
     </div>
   );
