6a5ee8024d1c7693c020736d18772785d9aa2d2e
David Paucar <soydavidpaucar@gmail.com>
2022-12-01 22:07:40 -0500

    chore: add form structure and style

--- src/components/TodoModal.tsx
+++ src/components/TodoModal.tsx
@@ -1,4 +1,5 @@
 import { MdOutlineClose } from 'react-icons/md';
+import { Button } from './Button';
 
 function TodoModal() {
   return (
@@ -7,7 +8,37 @@ function TodoModal() {
         <div className="absolute top-[-10px] right-0 translate-y-[-100%] text-[25px] p-[.5px] cursor-pointer bg-[#8892b0]/[.40] text-[#ccd6f6] rounded backdrop-blur-sm flex items-center justify-center transition-all z-0 hover:bg-red-600/[.80]">
           <MdOutlineClose />
         </div>
-        <h1>Todo Modal</h1>
+        <form className="w-full">
+          <h2 className="text-[#ccd6f6] capitalize text-[20px] font-semibold mb-5">
+            Add TODO
+          </h2>
+          <label className="text-[#ccd6f6] text-base" htmlFor="title">
+            Title
+            <input
+              className="backdrop-blur-sm border-none w-full mt-[8px] mb-[20px] rounded p-[10px] bg-[#8892b0]/[.5] text-[#ccd6f6] outline-none"
+              type="text"
+              id="title"
+            />
+          </label>
+          <label className="text-[#ccd6f6] text-base" htmlFor="status">
+            Status
+            <select
+              className="backdrop-blur-sm border-none w-full mt-[8px] mb-[20px] rounded p-[10px] bg-[#8892b0]/[.5] text-[#ccd6f6] outline-none"
+              name="status"
+              id="status">
+              <option value="completed">Completed</option>
+              <option value="uncompleted">Uncompleted</option>
+            </select>
+          </label>
+          <div className="flex justify-start items-center mt-5 gap-4">
+            <Button variant="primary" type="submit">
+              Add
+            </Button>
+            <Button variant="secondary" type="button">
+              Cancel
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
