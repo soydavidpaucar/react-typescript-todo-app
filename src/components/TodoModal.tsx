5b4e2f2a3f5577b0584d7cba5d47b8c762a513e2
David Paucar <soydavidpaucar@gmail.com>
2022-12-01 23:43:03 -0500

    feat(functionality): add option to close modal and update semantic button

--- src/components/TodoModal.tsx
+++ src/components/TodoModal.tsx
@@ -1,13 +1,22 @@
 import { MdOutlineClose } from 'react-icons/md';
 import { Button } from './Button';
 
-function TodoModal() {
-  return (
+type TodoModalProps = {
+  showModal: boolean;
+  setShowModal: (showModal: boolean) => void;
+};
+
+function TodoModal({ showModal, setShowModal }: TodoModalProps) {
+  return showModal ? (
     <div className="fixed top-0 left-0 w-full h-full bg-black/[.3] z-[1] flex items-center justify-center">
       <div className="backdrop-blur-sm max-w-lg bg-[rgb(10,25,47)]/[.85] w-[90%] text-white m-auto flex items-center justify-center p-4 rounded relative">
-        <div className="absolute top-[-10px] right-0 translate-y-[-100%] text-[25px] p-[.5px] cursor-pointer bg-[#8892b0]/[.40] text-[#ccd6f6] rounded backdrop-blur-sm flex items-center justify-center transition-all z-0 hover:bg-red-600/[.80]">
+        <button
+          type="button"
+          className="absolute top-[-10px] right-0 translate-y-[-100%] text-[25px] p-[.5px] cursor-pointer bg-[#8892b0]/[.40] text-[#ccd6f6] rounded backdrop-blur-sm flex items-center justify-center transition-all z-0 hover:bg-red-600/[.80]"
+          onClick={() => setShowModal(!showModal)}
+          onKeyDown={() => setShowModal(!showModal)}>
           <MdOutlineClose />
-        </div>
+        </button>
         <form className="w-full">
           <h2 className="text-[#ccd6f6] capitalize text-[20px] font-semibold mb-5">
             Add TODO
@@ -34,14 +43,18 @@ function TodoModal() {
             <Button variant="primary" type="submit">
               Add
             </Button>
-            <Button variant="secondary" type="button">
+            <Button
+              variant="secondary"
+              type="button"
+              onClick={() => setShowModal(!showModal)}
+              onKeyDown={() => setShowModal(!showModal)}>
               Cancel
             </Button>
           </div>
         </form>
       </div>
     </div>
-  );
+  ) : null;
 }
 
 export default TodoModal;
