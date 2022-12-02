36d48f741d1d4be710d89fc5096daea9e3a05933
David Paucar <soydavidpaucar@gmail.com>
2022-12-01 23:43:58 -0500

    feat(modal): add close modal state to send to child buttons and render the respective modal

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { Button, SelectButton } from './Button';
+import TodoModal from './TodoModal';
 
 function AppHeader() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   return (
     <div className="flex items-center justify-between h-[60px]">
-      <Button type="button" variant="primary">
+      <Button
+        type="button"
+        variant="primary"
+        onClick={() => setShowModal(!showModal)}>
         Add Task
       </Button>
       <SelectButton>
@@ -11,6 +18,7 @@ function AppHeader() {
         <option value="completed">Completed</option>
         <option value="uncompleted">Uncompleted</option>
       </SelectButton>
+      <TodoModal showModal={showModal} setShowModal={setShowModal} />
     </div>
   );
 }
