4f498a51f1b1f2807881ba1e4bf4fa362574a6f0
David Paucar <soydavidpaucar@gmail.com>
2022-12-03 16:47:09 -0500

    chore(store): add the provider to use the todo data globally

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { Provider } from 'react-redux';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
+import './index.css';
+import App from './App';
+import { store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
