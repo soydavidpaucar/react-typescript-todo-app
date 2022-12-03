2e6a8284ecc7935b9148b247ea725c28d46473a3
David Paucar <soydavidpaucar@gmail.com>
2022-12-03 18:50:18 -0500

    style(toast): add toast container with style for success and error

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { ToastContainer } from 'react-toastify';
 import PageTitle from './components/PageTitle';
 import AppHeader from './components/AppHeader';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+const contextClass = {
+  success: 'backdrop-blur bg-[#8892b0]/[.6] text-[#ccd6f6] font-sans text-sm',
+  error: 'backdrop-blur bg-red-600/[.60] text-[#ccd6f6] font-sans text-sm',
+};
 
 function App() {
   return (
-    <div className="bg-[#0a192f]">
-      <div className="max-w-7xl w-[90%] m-auto">
-        <PageTitle>TODO LIST</PageTitle>
-        <div className="w-full max-w-[750px] m-auto">
-          <AppHeader />
+    <>
+      <div className="bg-[#0a192f] h-screen">
+        <div className="max-w-7xl w-[90%] m-auto">
+          <PageTitle>TODO LIST</PageTitle>
+          <div className="w-full max-w-[750px] m-auto">
+            <AppHeader />
+          </div>
         </div>
       </div>
-    </div>
+      <ToastContainer
+        toastClassName={({ type }) =>
+          `${
+            contextClass[type || 'default']
+          } relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer mb-5`
+        }
+      />
+    </>
   );
 }
 
