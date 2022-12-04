1ed6390e978eaf8625adea96ddb85696880daf32
David Paucar <soydavidpaucar@gmail.com>
2022-12-04 17:09:22 -0500

    feat: :art: add filtered todos to return the selected filtered option

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -16,16 +16,25 @@ type TodoState = {
 function AppContent() {
   const todos = useSelector((state: { todos: TodoState }) => state.todos.todos);
 
+  const filter = useSelector((state) => state.todos.filter);
+
   const sortedTodos = [...todos].sort((a, b) => {
     const aDate = new Date(a.time);
     const bDate = new Date(b.time);
     return bDate.getTime() - aDate.getTime();
   });
 
+  const filteredTodos = sortedTodos.filter((todo) => {
+    if (filter === 'all') {
+      return true;
+    }
+    return todo.status === filter;
+  });
+
   return (
     <div className="bg-[#112240]/50 p-5">
-      {sortedTodos && sortedTodos.length > 0 ? (
-        sortedTodos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+      {filteredTodos && filteredTodos.length > 0 ? (
+        filteredTodos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
       ) : (
         <p className="text-base text-[#ccd6f6] text-center m-auto py-1 px-3 bg-[#8892b0]/[.20] backdrop-blur rounded max-w-fit">
           No tasks found!
