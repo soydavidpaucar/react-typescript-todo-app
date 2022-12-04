3f623b485fe3f6f5f59210d9aef279125da4b8f5
David Paucar <soydavidpaucar@gmail.com>
2022-12-03 20:14:49 -0500

    style(font): add new font weight

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
