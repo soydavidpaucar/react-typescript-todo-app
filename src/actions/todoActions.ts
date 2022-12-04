06f3acca550356569913a301fa50e96ac02265bf
David Paucar <soydavidpaucar@gmail.com>
2022-12-03 20:14:05 -0500

    chore: add delete todo action

--- src/actions/todoActions.ts
+++ src/actions/todoActions.ts
@@ -1,5 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+interface Todo {
+  id: string;
+  title: string;
+  status: 'completed' | 'uncompleted';
+  time: string;
+}
+
 const getInitialTodos = () => {
   const savedTodos = localStorage.getItem('todos');
   return savedTodos
@@ -26,8 +33,18 @@ const todosSlice = createSlice({
         localStorage.setItem('todos', JSON.stringify([{ ...action.payload }]));
       }
     },
+    deleteTodo: (state, action) => {
+      const todos = localStorage.getItem('todos');
+      if (todos) {
+        const parsedTodos = JSON.parse(todos);
+        const filteredTodos = parsedTodos.filter(
+          (todo: Todo) => todo.id !== action.payload
+        );
+        localStorage.setItem('todos', JSON.stringify(filteredTodos));
+      }
+    },
   },
 });
 
-export const { addTodo } = todosSlice.actions;
+export const { addTodo, deleteTodo } = todosSlice.actions;
 export default todosSlice.reducer;
