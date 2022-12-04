91056a9565556754d9c6ded0ec0b8f0c4c184e94
David Paucar <soydavidpaucar@gmail.com>
2022-12-04 11:26:49 -0500

    feat: :art: import app content and improve container style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { ToastContainer } from 'react-toastify';
-import PageTitle from './components/PageTitle';
-import AppHeader from './components/AppHeader';
 import 'react-toastify/dist/ReactToastify.min.css';
+
+import { ToastContainer } from 'react-toastify';
+
+import AppContent from './components/AppContent';
+import AppHeader from './components/AppHeader';
+import PageTitle from './components/PageTitle';
 
 const contextClass = {
   success: 'backdrop-blur bg-[#8892b0]/[.6] text-[#ccd6f6] font-sans text-sm',
@@ -11,11 +14,12 @@ const contextClass = {
 function App() {
   return (
     <>
-      <div className="bg-[#0a192f] h-screen">
+      <div className="bg-[#0a192f] min-h-screen h-full">
         <div className="max-w-7xl w-[90%] m-auto">
           <PageTitle>TODO LIST</PageTitle>
           <div className="w-full max-w-[750px] m-auto">
             <AppHeader />
+            <AppContent />
           </div>
         </div>
       </div>
