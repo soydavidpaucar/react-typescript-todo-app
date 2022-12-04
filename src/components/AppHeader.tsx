452c011b3a3ca38fc719c73027ca4527bc6b9d25
David Paucar <soydavidpaucar@gmail.com>
2022-12-04 16:59:45 -0500

    feat: :art: add filter status to the selector

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { updateFilter } from '../actions/todoActions';
 import { Button, SelectButton } from './Button';
 import TodoModal from './TodoModal';
 
 function AppHeader() {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const filterStatus = useSelector((state) => state.todos.filter);
+
+  const dispatch = useDispatch();
+
+  const handleFilter = ({ target }: Event) => {
+    const { value } = target as HTMLInputElement;
+    dispatch(updateFilter(value));
+  };
 
   return (
     <div className="flex items-center justify-between h-[60px]">
@@ -14,7 +24,7 @@ function AppHeader() {
         onClick={() => setShowModal(!showModal)}>
         Add Task
       </Button>
-      <SelectButton>
+      <SelectButton value={filterStatus} onChange={handleFilter}>
         <option value="all">All</option>
         <option value="completed">Completed</option>
         <option value="uncompleted">Uncompleted</option>
